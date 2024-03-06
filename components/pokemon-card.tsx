f8ff2bec9566ed49e0687afefde53c384ae28d6e
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-03-06 18:47:40 +0700

    pokemon api ya

--- components/pokemon-card.tsx
+++ components/pokemon-card.tsx
@@ -11,6 +11,7 @@ export function PokemonCard({ name }: PokemonCardProps) {
       className="group rounded-lg border m-3 px-5 py-4 bg-gray-200 hover:bg-gray-300"
       key={name + "Card"}
     >
+      <div></div>
       <h2 className={`md:text-2xl text-lg font-semibold`}>
         {name.charAt(0).toUpperCase() + name.slice(1)}
       </h2>
